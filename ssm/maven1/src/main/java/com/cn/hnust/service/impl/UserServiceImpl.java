d4de879c12086041ed221ba13e902bdcb25af15c
chenv <chen-v@foxmail.com>
2016-06-07 19:30:14 +0800

    Modified   ssm/maven1/.settings/org.eclipse.wst.common.component Modified   ssm/maven1/logs/ssm.log Modified   ssm/maven1/src/main/java/com/cn/hnust/mapping/UserMapper.xml Modified   ssm/maven1/src/main/java/com/cn/hnust/service/impl/UserServiceImpl.java Modified   ssm/maven1/src/main/resources/jdbc.properties Modified   ssm/maven1/src/test/java/org/zsl/testmybatis/TestMyBatis.java

--- ssm/maven1/src/main/java/com/cn/hnust/service/impl/UserServiceImpl.java
+++ ssm/maven1/src/main/java/com/cn/hnust/service/impl/UserServiceImpl.java
@@ -1,16 +1,16 @@
 package com.cn.hnust.service.impl;
 
-import javax.annotation.Resource;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.cn.hnust.dao.IUserDao;
 import com.cn.hnust.pojo.User;
 import com.cn.hnust.service.IUserService;
 
-@Service("userService")
+@Service
 public class UserServiceImpl implements IUserService {
-	@Resource
+	@Autowired
 	private IUserDao userDao;
 	@Override
 	public User getUserById(int userId) {
--- ssm/maven1/src/test/java/org/zsl/testmybatis/TestMyBatis.java
+++ ssm/maven1/src/test/java/org/zsl/testmybatis/TestMyBatis.java
@@ -17,7 +17,6 @@ import com.cn.hnust.service.IUserService;
 
 @RunWith(SpringJUnit4ClassRunner.class)		//表示继承了SpringJUnit4ClassRunner类
 @ContextConfiguration(locations = {"classpath:spring-mybatis.xml"})
-
 public class TestMyBatis {
 	private static Logger logger = Logger.getLogger(TestMyBatis.class);
 //	private ApplicationContext ac = null;
