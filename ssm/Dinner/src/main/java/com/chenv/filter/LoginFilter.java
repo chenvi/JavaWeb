f6f29db68b76e246a60b1b8ba1ff283391b4b24e
chenv <chen-v@foxmail.com>
2016-12-14 11:21:17 +0800

    Add        ssm/Dinner/src/main/java/com/chenv/filter/LoginFilter.java

---
+++ ssm/Dinner/src/main/java/com/chenv/filter/LoginFilter.java
@@ -0,0 +1,60 @@
+package com.chenv.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class LoginFilter implements Filter {
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void doFilter(ServletRequest arg0, ServletResponse arg1,
+			FilterChain arg2) throws IOException, ServletException {
+		HttpServletRequest request = (HttpServletRequest) arg0;
+		HttpServletResponse response = (HttpServletResponse) arg1;
+		HttpSession session = request.getSession();
+		//访问的uri
+		String path = request.getRequestURI();
+		
+		//无需过滤login页面
+//		if (path.indexOf("/login.jsp") > -1) {
+//			arg2.doFilter(request, response);
+//		}
+//		if (path.indexOf("/index.jsp") > -1) {
+//			arg2.doFilter(request, response);
+//		}
+//		if (path.indexOf("/account.jsp") > -1) {
+//			arg2.doFilter(request, response);
+//			return;
+//		}
+		
+		if (session.getAttribute("userName") == null) {
+//        	request.setAttribute("msg", "请登录后操作！！");
+        	request.getRequestDispatcher("/login.jsp").forward(request, response);
+//            response.sendRedirect(request.getContextPath() + "/login.jsp");
+        }else {
+        	arg2.doFilter(request, response);
+        	return;
+		}
+	}
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
