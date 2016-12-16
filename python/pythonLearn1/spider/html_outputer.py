fb94c6cfddd2e4fd146887ba10b05aec20bd842e
chenv <chen-v@foxmail.com>
2016-12-16 14:44:59 +0800

    add python

---
+++ python/pythonLearn1/spider/html_outputer.py
@@ -0,0 +1,6 @@
+class HtmlOutputer(object):
+    def collect(self, new_data):
+        pass
+
+    def output_html(self):
+        pass
---
+++ python/pythonLearn1/spider/html_parser.py
@@ -0,0 +1,36 @@
+import urllib.parse
+from bs4 import BeautifulSoup
+import re
+class HtmlParser(object):
+    def _get_new_urls(self, url, soup):
+        new_urls = set()
+
+        links = soup.find_all('a',href=re.compile(r"/view/\d+\.htm"))
+        for link in links:
+            new_url = link['href']
+            new_full_url = urllib.parse.urljoin(url,new_url)
+            new_urls.add(new_full_url)
+        return new_urls
+
+    def _get_new_data(self, url, soup):
+        data = {}
+
+        title = soup.find('dd',class_="lemmaWgt-lemmaTitle-title").find('h1')
+        data['title'] = title.get_text()
+
+        summary = soup.find('div',class_ ="lemma-summary")
+        data['summary'] = summary.get_text()
+
+        data['url'] = url
+
+        return data
+
+    def parse(self, url, html_cont):
+        if url is None or html_cont is None:
+            return
+
+        soup = BeautifulSoup(html_cont,'html.parser',from_encoding='utf-8')
+        new_urls = self._get_new_urls(url,soup)
+        new_data = self._get_new_data(url,soup)
+        return new_urls,new_data
+
